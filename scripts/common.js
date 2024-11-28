512cdd65842fa5d6a85807bb5cf25447ebcd769d
Jonatan Lledo <117984204+jonatan-lledo-netcentric@users.noreply.github.com>
2024-11-28 18:03:27 +0100

    remove unnecesary falsy check #36

--- scripts/common.js
+++ scripts/common.js
@@ -335,7 +335,7 @@ async function getConstantValues() {
  * @returns {Object} An parsed object with those values and keys
  */
 export const extractObjectFromArray = (data) => {
-  if (!data || !Array.isArray(data)) return {};
+  if (!Array.isArray(data)) return {};
   const obj = {};
   for (const item of data) {
     try {
@@ -458,7 +458,7 @@ export const getJsonFromUrl = async (route) => {
  *                     original input.
  */
 export const formatStringToArray = (inputString) => {
-  if (!inputString || typeof inputString !== 'string') return [];
+  if (typeof inputString !== 'string') return [];
   // eslint-disable-next-line no-useless-escape
   const cleanedString = inputString.replace(/[\[\]\\'"]+/g, '');
   return cleanedString.split(',')
