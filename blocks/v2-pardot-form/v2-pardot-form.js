fa744b424df19e731dfe05d5eb16f42c901b7a55
santi-homps <santiagohomps@gmail.com>
2025-02-14 10:20:37 -0300

    last refactor

--- blocks/v2-pardot-form/v2-pardot-form.js
+++ blocks/v2-pardot-form/v2-pardot-form.js
@@ -1,6 +1,6 @@
 import { loadScript, sampleRUM } from '../../scripts/aem.js';
 import { getTextLabel, createElement } from '../../scripts/common.js';
-import { getAsyncCustomDropdown, addDropdownInteraction } from '../../../common/custom-dropdown/custom-dropdown.js';
+import { getCustomDropdown, addDropdownInteraction } from '../../../common/custom-dropdown/custom-dropdown.js';
 
 const blockName = 'v2-pardot-form';
 
@@ -95,7 +95,6 @@ function generateUnique() {
 
 function constructPayload(form) {
   const payload = { __id__: generateUnique() };
-  console.log(payload);
   [...form.elements].forEach((fe) => {
     if (fe.name) {
       if (fe.type === 'radio' && fe.checked) {
@@ -359,7 +358,7 @@ async function createCustomDropdown(fd) {
     name: fd.Name,
     mandatory: fd.Mandatory,
   };
-  const customDropdown = await getAsyncCustomDropdown(configFd);
+  const customDropdown = await getCustomDropdown(configFd);
   return document.createRange().createContextualFragment(customDropdown);
 }
 
