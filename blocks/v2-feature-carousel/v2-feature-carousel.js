ee9e446627232f1f363f431d8781d0ac5e96db0f
santi-homps <santiagohomps@gmail.com>
2025-04-01 12:53:05 -0300

    move classes and arrow logic

--- blocks/v2-feature-carousel/v2-feature-carousel.js
+++ blocks/v2-feature-carousel/v2-feature-carousel.js
@@ -2,10 +2,42 @@ import { adjustPretitle, createElement, decorateIcons, getTextLabel } from '../.
 import { listenScroll, createArrowControls, setCarouselPosition } from '../../scripts/carousel-helper.js';
 
 const blockName = 'v2-feature-carousel';
+const CLASSES = {
+  button: `${blockName}__button`,
+  buttonPrev: `${blockName}__button-prev`,
+  buttonNext: `${blockName}__button-next`,
+  imageWrapper: `${blockName}__image-wrapper`,
+  image: `${blockName}__image`,
+  listWrapper: `${blockName}__list-wrapper`,
+  listContainer: `${blockName}__list-container`,
+  listContainerSingle: `${blockName}__list-container--single`,
+  list: `${blockName}__list`,
+  listItem: `${blockName}__list-item`,
+  title: `${blockName}__title`,
+  arrowcontrols: `${blockName}__arrowcontrols`,
+};
 
-const updateImage = (el) => {
+const updateArrows = (el, index) => {
+  let arrowControl = el.parentElement.previousElementSibling.querySelector(`.${CLASSES.button}:disabled`);
+
+  if (arrowControl) {
+    arrowControl.disabled = false;
+    arrowControl = null;
+  }
+  // disable arrow buttons
+  if (index === 0) {
+    arrowControl = el.parentElement.previousElementSibling.querySelector(`.${CLASSES.buttonPrev}`);
+  } else if (index === el.parentNode.children.length - 1) {
+    arrowControl = el.parentElement.previousElementSibling.querySelector(`.${CLASSES.buttonNext}`);
+  }
+  if (arrowControl) {
+    arrowControl.disabled = true;
+  }
+};
+
+const updateOtherCarousel = (el) => {
   const activeBlock = el.closest('.block');
-  const allImages = activeBlock.querySelectorAll(`.${blockName}__image`);
+  const allImages = activeBlock.querySelectorAll(`.${CLASSES.image}`);
 
   if (allImages.length > 1) {
     const index = Number(el.dataset.index);
@@ -21,22 +53,9 @@ const updateActiveClass = (elements, entry) => {
   elements.forEach((el, index) => {
     if (el === entry.target) {
       el.classList.add('active');
-      updateImage(el);
-      let arrowControl = el.parentElement.previousElementSibling.querySelector(`.${blockName}__button:disabled`);
 
-      if (arrowControl) {
-        arrowControl.disabled = false;
-        arrowControl = null;
-      }
-      // disable arrow buttons
-      if (index === 0) {
-        arrowControl = el.parentElement.previousElementSibling.querySelector(`.${blockName}__button-prev`);
-      } else if (index === el.parentNode.children.length - 1) {
-        arrowControl = el.parentElement.previousElementSibling.querySelector(`.${blockName}__button-next`);
-      }
-      if (arrowControl) {
-        arrowControl.disabled = true;
-      }
+      updateOtherCarousel(el);
+      updateArrows(el, index);
     } else {
       el.classList.remove('active');
     }
@@ -45,12 +64,12 @@ const updateActiveClass = (elements, entry) => {
 
 const arrowFragment = () =>
   document.createRange().createContextualFragment(`<li>
-  <button aria-label="${getTextLabel('Previous')}" class="${blockName}__button ${blockName}__button-prev">
+  <button aria-label="${getTextLabel('Previous')}" class="${CLASSES.button} ${CLASSES.buttonPrev}">
     <span class="icon icon-arrow-right" />
   </button>
 </li>
 <li>
-  <button aria-label="${getTextLabel('Next')}" class="${blockName}__button ${blockName}__button-next">
+  <button aria-label="${getTextLabel('Next')}" class="${CLASSES.button} ${CLASSES.buttonNext}">
     <span class="icon icon-arrow-right" />
   </button>
 </li>`);
@@ -58,7 +77,7 @@ const arrowFragment = () =>
 const createImageList = (pictures) => {
   let images = '';
   pictures.forEach((pic, idx) => {
-    pic.classList.add(`${blockName}__image`);
+    pic.classList.add(`${CLASSES.image}`);
     pic.dataset.index = idx + 1;
     images += pic.outerHTML;
   });
@@ -73,12 +92,12 @@ const createCardsList = (nodes) => {
       btn.classList.add('button--large');
     });
 
-    const li = createElement('li', { classes: `${blockName}__list-item` });
+    const li = createElement('li', { classes: `${CLASSES.listItem}` });
     li.innerHTML = node.innerHTML;
     li.dataset.index = idx + 1;
 
     const headings = li.querySelectorAll('h1, h2, h3, h4');
-    [...headings].forEach((heading) => heading.classList.add(`${blockName}__title`));
+    [...headings].forEach((heading) => heading.classList.add(`${CLASSES.title}`));
 
     adjustPretitle(li);
     cardItems += li.outerHTML;
@@ -100,26 +119,26 @@ export default async function decorate(block) {
   });
 
   const carousel = document.createRange().createContextualFragment(`
-    <div class='${blockName}__image-wrapper'>
+    <div class='${CLASSES.imageWrapper}'>
       ${createImageList(pictureNodes)}
     </div>
-    <div class='${blockName}__list-wrapper'>
-      <div class='${blockName}__list-container'>
-        <ul class='${blockName}__list'>
+    <div class='${CLASSES.listWrapper}'>
+      <div class='${CLASSES.listContainer}'>
+        <ul class='${CLASSES.list}'>
           ${createCardsList(otherNodes)}
         </ul>
       </div>
     </div>
   `);
 
-  const carouselList = carousel.querySelector(`.${blockName}__list`);
+  const carouselList = carousel.querySelector(`.${CLASSES.list}`);
 
   if (otherNodes.length > 1) {
-    createArrowControls(carouselList, `.${blockName}__list-item.active`, [`${blockName}__arrowcontrols`], arrowFragment());
-    const elements = carouselList.querySelectorAll(`.${blockName}__list-item`);
+    createArrowControls(carouselList, `.${CLASSES.listItem}.active`, [`${CLASSES.arrowcontrols}`], arrowFragment());
+    const elements = carouselList.querySelectorAll(`.${CLASSES.listItem}`);
     listenScroll(carouselList, elements, updateActiveClass, 0.75);
   } else {
-    carouselList.parentElement.classList.add(`${blockName}__list-container--single`);
+    carouselList.parentElement.classList.add(`${CLASSES.listContainerSingle}`);
   }
 
   block.innerHTML = '';
