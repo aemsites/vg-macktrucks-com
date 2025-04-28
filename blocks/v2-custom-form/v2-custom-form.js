89c5f48c9d5b7e211893989301af6ee0ff1f622a
Alexis Coelho <alexis.coelho@gmail.com>
2025-04-28 09:45:44 +0100

    Persist type email on the form input field #260

--- blocks/v2-custom-form/v2-custom-form.js
+++ blocks/v2-custom-form/v2-custom-form.js
@@ -241,8 +241,7 @@ function createSubmit(fd) {
 function createInput(fd) {
   const input = createElement('input', {
     props: {
-      // If the field is an email with a custom pattern, use 'text' to avoid conflicts with browser validation.
-      type: fd.Type === 'email' && fd.Pattern ? 'text' : fd.Type,
+      type: fd.Type,
     },
   });
   setPlaceholder(input, fd);
