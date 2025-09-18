c29f987b88f77c971d449419766bff358b2ce4b9
amglebihan <166131459+amglebihan@users.noreply.github.com>
2025-09-18 11:10:21 +0200

    441 - Add Transport Canada Issue Date

--- blocks/vin-number/vin-number.js
+++ blocks/vin-number/vin-number.js
@@ -24,6 +24,10 @@ const valueDisplayList = [
     key: 'recall_date',
   },
   {
+    key: 'tc_recall_date',
+    frenchKey: 'tc_recall_date_french',
+  },
+  {
     key: 'mfr_recall_number',
   },
   {
