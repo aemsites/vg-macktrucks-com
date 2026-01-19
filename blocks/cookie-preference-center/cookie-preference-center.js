01e874e69ef59a781efd450b515725c02b610fca
ESQUIVEL <917876@cognizant.com>
2026-01-19 12:23:36 -0300

    Changes on the button of cookie preference center to be using a listener

--- blocks/cookie-preference-center/cookie-preference-center.js
+++ blocks/cookie-preference-center/cookie-preference-center.js
@@ -1,8 +1,13 @@
 import { createElement } from '../../scripts/common.js';
 
 export default function decorate(block) {
-  const button = createElement('button', { classes: 'cta', props: { onclick: 'OneTrust.ToggleInfoDisplay();' } });
+  const button = createElement('button', { classes: 'cta' });
   button.textContent = block.textContent;
+  button.addEventListener('click', () => {
+    if (window.OneTrust) {
+      window.OneTrust.ToggleInfoDisplay();
+    }
+  });
 
   block.textContent = '';
   block.append(button);
