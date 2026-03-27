6c1e38e1ed41f163bc03ddeabf0383ce773108f1
Alexis Coelho <alexis.coelho@gmail.com>
2026-03-27 10:51:25 +0100

    133078 - Fix 404 script execution

--- scripts/404-entrypoint.js
+++ scripts/404-entrypoint.js
@@ -7,34 +7,32 @@ import './scripts.js';
 window.isErrorPage = true;
 window.errorCode = '404';
 
-window.addEventListener('load', () => {
-  sampleRUM('404', { source: document.referrer, target: window.location.href });
+sampleRUM('404', { source: document.referrer, target: window.location.href });
 
-  if (document.referrer) {
-    const { origin, pathname } = new URL(document.referrer);
-    if (origin === window.location.origin) {
-      const main = document.querySelector('main.error');
+if (document.referrer) {
+  const { origin, pathname } = new URL(document.referrer);
+  if (origin === window.location.origin) {
+    const main = document.querySelector('main.error');
 
-      const observer = new MutationObserver((mutations, observer) => {
-        const fragmentContainer = document.querySelector('.fragment-wrapper');
-        if (fragmentContainer) {
-          const backLinkTitle = getTextLabel('404:back_link_title');
-          const btnContainer = document.createElement('p');
-          const backBtn = document.createElement('a');
-          backBtn.classList.add('button', 'button--primary');
-          backBtn.href = pathname;
-          backBtn.textContent = backLinkTitle;
-          backBtn.title = backLinkTitle;
-          btnContainer.append(backBtn);
-          fragmentContainer.append(btnContainer);
-          observer.disconnect();
-        }
-      });
+    const observer = new MutationObserver((mutations, observer) => {
+      const fragmentContainer = document.querySelector('.fragment-wrapper');
+      if (fragmentContainer) {
+        const backLinkTitle = getTextLabel('404:back_link_title');
+        const btnContainer = document.createElement('p');
+        const backBtn = document.createElement('a');
+        backBtn.classList.add('button', 'button--primary');
+        backBtn.href = pathname;
+        backBtn.textContent = backLinkTitle;
+        backBtn.title = backLinkTitle;
+        btnContainer.append(backBtn);
+        fragmentContainer.append(btnContainer);
+        observer.disconnect();
+      }
+    });
 
-      observer.observe(main, {
-        childList: true,
-        subtree: true,
-      });
-    }
+    observer.observe(main, {
+      childList: true,
+      subtree: true,
+    });
   }
-});
+}
