4bbd4644acea7e6f48d31baa37fae75be77f1b0b
Iliam Pavkovic <iliam.pavkovic@volvo.com>
2026-04-07 09:10:57 -0300

    134927 - Bug: Cookie page is outdated

--- blocks/cookie-preference-center/cookie-preference-center.js
+++ blocks/cookie-preference-center/cookie-preference-center.js
@@ -1,7 +1,9 @@
 import { createElement } from '../../scripts/common.js';
 
 export default function decorate(block) {
-  const button = createElement('button', { classes: 'cta' });
+  const button = createElement('button', {
+    classes: ['button', 'button--primary'],
+  });
   button.textContent = block.textContent;
   button.addEventListener('click', () => {
     if (window.OneTrust) {
